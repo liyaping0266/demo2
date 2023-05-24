8f07f5745da143d707c6db9f301598a070f6a591
liyaping0266 <liyaping0266@163.com>
2023-05-24 19:52:28 +0800

    第一次修改

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,5 +5,6 @@ public class Main {
     public static void main(String[] args) {
 	// write your code here
         System.out.println("Hello World! --add content");
+        //第一次修改，提交并推送至远程Github仓库上
     }
 }
